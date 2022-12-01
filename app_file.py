11564d5d4a9c93951183c1e70229bf710d837b01
rishabh-dsai <118420549+rishabh-dsai@users.noreply.github.com>
2022-12-01 12:24:00 +0530

    Update app_file.py

--- app_file.py
+++ app_file.py
@@ -68,7 +68,8 @@ with st.expander("Correlation Heatmap & Feature Impact Analysis"):
     st.write(show_df)
     st.write("")
     st.write("Visualizing it in graph")
-    line_ch_sch=px.bar(x=show_df.index,y=show_df['Predicted Pass Percentage'],markers=True,\
+    chart_df=show_df[['Predicted Pass Percentage']]
+    line_ch_sch=px.bar(chart_df,markers=True,\
                        title="Impact of Features")
     st.plotly_chart(line_ch_sch,use_container_width=True)
     
